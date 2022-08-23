e38945f3138c64207f5dfe88a48d8f6a9aa187b1
intellieseawolf <intelliseawolf103@gmail.com>
2022-08-23 21:25:51 +0900

    fixed: http to https

--- server.js
+++ server.js
@@ -4,7 +4,8 @@
  * @description This file is responsible for connecting to mongoDB, creating an HTTP server and adding routes.
  * Server is created by binding express app instance.
  * */
-const { createServer } = require("http");
+const https = require("https");
+const fs = require("fs");
 const { log } = require("./errors");
 const { constants } = require("./config");
 const { app } = require("./app");
@@ -14,21 +15,27 @@ const { PORT, LOG_LEVELS } = constants;
 
 connectToMongoDb();
 
-const server = createServer(app);
+const server = https.createServer(
+  {
+    key: fs.readFileSync("key.pem"),
+    cert: fs.readFileSync("cert.pem"),
+  },
+  app
+);
 
 // Event listeners to catch uncaught errors
-process.on("unhandledRejection", error => {
-    log(LOG_LEVELS.ERROR, error.message, { time: new Date() });
-    process.exit(1);
+process.on("unhandledRejection", (error) => {
+  log(LOG_LEVELS.ERROR, error.message, { time: new Date() });
+  process.exit(1);
 });
 
-process.on("exit", code => {
-    console.log(`Exiting with code: ${code}`);
+process.on("exit", (code) => {
+  console.log(`Exiting with code: ${code}`);
 });
 
-server.listen(PORT, err => {
-    if (err) {
-        return console.log(`Something went wrong: \n${err}`);
-    }
-    console.log(`Server is listening on port: ${PORT}`);
+server.listen(PORT, (err) => {
+  if (err) {
+    return console.log(`Something went wrong: \n${err}`);
+  }
+  console.log(`Server is listening on port: ${PORT}`);
 });
